6b843e9561174005c5f7113ab46d18d47f8e21b7
aechamjae <aechamjaehmj@gmail.com>
2026-09-10 00:57:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gomupal-v1";
+const CACHE = "gomupal-v2";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e)=>{
